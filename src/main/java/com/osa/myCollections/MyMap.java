8e4c7450e7c5c62e0aacf8ec3bbe6460cc578c20
mahfuz <20mahfuzahmed20@gmail.com>
2021-07-19 00:44:17 -0400

    HashMap

--- src/main/java/com/osa/myCollections/MyMap.java
+++ src/main/java/com/osa/myCollections/MyMap.java
@@ -10,17 +10,19 @@ public class MyMap {
 		
 		
 		Map<Integer,String> map = new HashMap<Integer,String>();
-		
+		map.put(0, "Iftikhar");
 		map.put(1, "uddin");
 		map.put(2, "Akbar");
 		map.put(3, "Mahfuz");
 		map.put(null, null);
 		
-		System.out.println(map);
-		System.out.println(map.keySet());
-		System.out.println(map.values());
+		System.out.println("map = "+map);
+		System.out.println("map.keySet() = "+map.keySet());
+		System.out.println("map.values() = "+map.values());
+		System.out.println("************************************************");
 		
-		for(Map.Entry<Integer,String> entry:map.entrySet()) {
+		for(Map.Entry<Integer,String> entry : map.entrySet()) {
+			
 			System.out.println(entry.getKey()+"    "+entry.getValue());
 		}
 	}
