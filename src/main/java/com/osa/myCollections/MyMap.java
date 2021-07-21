59066c4161da732bbf465136ee38065990fc297e
mahfuz <20mahfuzahmed20@gmail.com>
2021-07-21 13:05:26 -0400

    HashMapForLoop

--- src/main/java/com/osa/myCollections/MyMap.java
+++ src/main/java/com/osa/myCollections/MyMap.java
@@ -15,16 +15,28 @@ public class MyMap {
 		map.put(2, "Akbar");
 		map.put(3, "Mahfuz");
 		map.put(null, null);
-		
 		System.out.println("map = "+map);
+		System.out.println("map.entrySet() = "+map.entrySet());
 		System.out.println("map.keySet() = "+map.keySet());
 		System.out.println("map.values() = "+map.values());
+		System.out.println("map.get(1) = "+map.get(1));
+		System.out.println("map.replace(2, \"Akbar\", \"Ahmed\") "+map.replace(2, "Akbar", "Ahmed"));
 		System.out.println("************************************************");
 		
 		for(Map.Entry<Integer,String> entry : map.entrySet()) {
 			
 			System.out.println(entry.getKey()+"    "+entry.getValue());
 		}
+		System.out.println("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$");
+		
+		for (Integer i : map.keySet()) {
+		      System.out.println("key: " + i + " value: " + map.get(i));
+		    }
+		System.out.println("###################################################");
+		
+//		for (String i : k.keySet()) {
+//		      System.out.println("key: " + i + " value: " + k.get(i));
+//		    }
 	}
 
 }
